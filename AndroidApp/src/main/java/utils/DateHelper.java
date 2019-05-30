080e990ef0310bb7b46930e4c7d82aeb834eebca
mayanksu <mayank.suneja@mobikwik.com>
2019-05-30 12:34:46 +0530

    # Added datehelper method

--- AndroidApp/src/main/java/utils/DateHelper.java
+++ AndroidApp/src/main/java/utils/DateHelper.java
@@ -1,5 +1,7 @@
 package main.java.utils;
 
+import logger.Log;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -71,6 +73,57 @@ public abstract class DateHelper {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
         return dateFormat.format(date);
+    }
+
+    public static String getMonthInStringFromInteger(int month) {
+
+        String monthString;
+        switch (month)
+
+        {
+            case 1:
+                monthString = "January";
+                break;
+            case 2:
+                monthString = "February";
+                break;
+            case 3:
+                monthString = "March";
+                break;
+            case 4:
+                monthString = "April";
+                break;
+            case 5:
+                monthString = "May";
+                break;
+            case 6:
+                monthString = "June";
+                break;
+            case 7:
+                monthString = "July";
+                break;
+            case 8:
+                monthString = "August";
+                break;
+            case 9:
+                monthString = "September";
+                break;
+            case 10:
+                monthString = "October";
+                break;
+            case 11:
+                monthString = "November";
+                break;
+            case 12:
+                monthString = "December";
+                break;
+            default:
+                monthString = "Invalid month";
+                break;
+        }
+
+        Log.info(monthString);
+        return monthString;
     }
 
     public static int getCurrentMonthInteger() {
