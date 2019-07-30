027d32c8d6d9a8f9e5ea7e44e931d75396caaa27
parajjain <paraj.jain@mobikwik.com>
2019-07-30 16:48:50 +0530

    IMPS Done

---
+++ WebAutomation/src/main/java/PageObject/BusPage.java
@@ -0,0 +1,43 @@
+package PageObject;
+
+import common.DateHelper;
+import org.openqa.selenium.WebDriver;
+
+public class BusPage {
+
+    WebDriver driver;
+
+    //Heading for Visibility
+    //h1[@class= 'ft25 twhite smtop0 smbottom24']
+
+
+//    City From
+//label[text()= 'From']/following::input[1]
+
+
+    //select From City
+    //ng-dropdown-panel[@id= 'a6c5677fcc33']
+
+
+
+
+    //City To
+
+    //label[text()= 'From']/following::input[2]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
