2765e5766b93a3d3d2bb63ee31cc7c0e8c1efe6d
mayanksu <mayank.suneja@mobikwik.com>
2019-08-12 17:00:28 +0530

    Remove the sleep for IMPS OTP screen

--- WebAutomation/src/test/java/BankTransferFlow/BankTransferTest.java
+++ WebAutomation/src/test/java/BankTransferFlow/BankTransferTest.java
@@ -10,8 +10,8 @@ public class BankTransferTest extends TestBase {
     @Test(groups = {"bankTransferFlow"}, priority = 0, description = "Verify Bank Transfer Flow on Web")
     public void Test_Bank_Transfer_Flow() throws InterruptedException {
 
-//        LoginHelper loginHelper= new LoginHelper(driver);
-//        loginHelper.loginViaOtp("7795709569", "para jain", "par.ajjain@gmail.com", "7795709569");
+//        LoginHelper loginHelper = new LoginHelper(driver);
+//        loginHelper.loginViaOtp("8527797582", "T.C. Suneja", "mayank.suneja@mobikwik.com", "8527797582");
 
         BankTransferHelper bankTransferHelper = new BankTransferHelper(getWebDriver());
         bankTransferHelper.bankTransfer("Mayank Suneja", "114601503265", "ICIC0001146", "50", "Money sent successfully");
