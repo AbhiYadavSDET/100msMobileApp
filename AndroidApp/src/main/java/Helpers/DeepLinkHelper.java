084af5ce4611abdb27266d2a63bab6a3e07d86be
parajjain <paraj.jain@mobikwik.com>
2020-01-21 21:26:47 +0530

    MBK Deeplinks method in Deeplink Helper

--- AndroidApp/src/main/java/Helpers/DeepLinkHelper.java
+++ AndroidApp/src/main/java/Helpers/DeepLinkHelper.java
@@ -6,6 +6,8 @@ import UITestFramework.MBKPermissions;
 import UITestFramework.MBReporter;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import logger.Log;
 import org.openqa.selenium.By;
 import org.testng.annotations.Parameters;
@@ -13,7 +15,6 @@ import utils.Element;
 import utils.Screen;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class DeepLinkHelper {
@@ -23,7 +24,7 @@ public class DeepLinkHelper {
     HomePage homePage;
     Screen screen;
     PermissionHelper permissionHelper;
-
+    Process process;
     PermissionPage permissionPage;
 
 
@@ -35,6 +36,10 @@ public class DeepLinkHelper {
         permissionPage = new PermissionPage(driver);
         permissionHelper = new PermissionHelper(driver);
         homePage = new HomePage(driver);
+
+
+
+
 
     }
 
@@ -93,4 +98,149 @@ public class DeepLinkHelper {
         //Thread.sleep(2000);
         //driver.navigate().back();
     }
+
+    public void validateDeeplink(String deeplinkstring, String appName, String element, String elementType, String module) throws InterruptedException, IOException {
+
+//        mbkCommonControlsHelper.dismissAllOnHomePage(driver);
+
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+
+        Thread.sleep(3000);
+
+        String cmd = "/home/parajjain/Android/Sdk/platform-tools/adb shell am start -a android.intent.action.VIEW -d "+deeplinkstring;
+
+        process = Runtime.getRuntime().exec(cmd);
+
+//        driver.get(deeplinkstring);
+
+        Thread.sleep(6000);
+
+        boolean appAvaibility= getApplicableApp(appName);
+
+//        if(abc){
+//            Log.info("true");
+//        }else{
+//            Log.info("false");
+//        }
+
+        if(appAvaibility){
+
+            mbReporter.verifyTrueWithLogging(appAvaibility, "Deeplink Indexing of "+deeplinkstring+ " is available for "+appName, true, false );
+
+            Element.selectElement(driver, (AndroidElement) driver.findElement(By.xpath("//android.widget.TextView[@text= '"+appName+"']")), "Select "+appName+" App");
+
+            Element.selectElement(driver, (AndroidElement) driver.findElement(By.id("android:id/button_once")), "Open App for One time");
+
+            Thread.sleep(3000);
+
+//            if(Element.isElementPresent(driver, By.id("content"))){
+//
+//                driver.pressKey(new KeyEvent(AndroidKey.BACK));
+//            }
+
+//            Thread.sleep(2000);
+
+            if(elementType.equalsIgnoreCase("id")) {
+
+                mbReporter.verifyTrueWithLogging(Element.isElementPresent(driver, By.id(element)), "User is redirected to the " + module + " feature", true, false);
+
+            }else{
+
+                mbReporter.verifyTrueWithLogging(Element.isElementPresent(driver, By.xpath(element)), "User is redirected to the " + module + " feature", true, false);
+
+            }
+
+
+
+        }else {
+
+            mbReporter.verifyTrueWithLogging(appAvaibility, "Deeplink Indexing of "+deeplinkstring+ " is not available for "+appName, true, false );
+
+
+        }
+
+
+
+
+    }
+
+//    public void validateDeeplink(String deeplinkstring, String appName) throws InterruptedException, IOException {
+//
+//        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+//
+//        Thread.sleep(3000);
+//
+//        String cmd = "/home/parajjain/Android/Sdk/platform-tools/adb shell am start -a android.intent.action.VIEW -d "+deeplinkstring;
+//
+//        process = Runtime.getRuntime().exec(cmd);
+////
+//////        driver.get(deeplinkstring);
+////
+//        Thread.sleep(6000);
+//
+//        boolean abc= getApplicableApp(appName);
+////
+//////        if(abc){
+//////            Log.info("true");
+//////        }else{
+//////            Log.info("false");
+//////        }
+//
+//        if(!abc){
+//
+//            mbReporter.verifyTrueWithLogging(!abc, "Deeplink Indexing of "+deeplinkstring+ " is not available for "+appName, true, false );
+//
+//
+//        }else {
+//
+//            mbReporter.verifyTrueWithLogging(!abc, "Deeplink Indexing of "+deeplinkstring+ " is available for "+appName, true, false );
+//
+//        }
+//
+//
+//
+//
+//    }
+
+
+    public boolean getApplicableApp( String appName) throws InterruptedException{
+
+
+
+        List<AndroidElement> applicableApp= driver.findElements(By.id("android:id/text1"));
+
+        Thread.sleep(2000);
+        int noOfApp = applicableApp.size();
+
+        boolean abc = false;
+
+        for (int i = 0; i < noOfApp ; i++) {
+
+//            Log.info(applicableApp.get(i).getText());
+//            Log.info(appName);
+
+            String appInstance= applicableApp.get(i).getText();
+
+            if(appInstance.equals(appName)){
+                System.out.println("Deeplink Indexing available for "+ appName);
+                abc=true;
+                break;
+
+            }
+
+        }
+
+//        if(abc){
+//            Log.info("true found");
+//        }else{
+//            Log.info("false not found");
+//        }
+        return abc;
+
+    }
+
+
+
+
+
 }
