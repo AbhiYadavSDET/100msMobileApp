303e4313419813e074fab44ca885dd4fe12f822c
parajjain <Kota@324008>
2021-06-17 23:49:55 +0530

    Login via Onboarding and Log Out Flow Done

--- AndroidApp/src/main/java/PageObject/OnboardingPage.java
+++ AndroidApp/src/main/java/PageObject/OnboardingPage.java
@@ -27,8 +27,55 @@ public class OnboardingPage {
     @AndroidFindBy(id = "phone_number")
     private AndroidElement textbox_mobile_no;
 
-    @AndroidFindBy(id = "send_otp")
+    @AndroidFindBy(id = "send_otp_btn")
     private AndroidElement cta_get_otp;
+
+    @AndroidFindBy(id = "edit_text_otp")
+    private AndroidElement textbox_enter_otp;
+
+    @AndroidFindBy(id = "verify_otp_btn")
+    private AndroidElement cta_submit_otp;
+
+    @AndroidFindBy(id = "tv_permission")
+    private AndroidElement permission_statement;
+
+    @AndroidFindBy(id="btn_continue")
+    private AndroidElement cta_continue_with_aadhaar;
+
+    @AndroidFindBy(id= "tv_right")
+    private AndroidElement cta_skip;
+
+    @AndroidFindBy(id="alertTitle")
+    private AndroidElement permission_consent;
+
+    @AndroidFindBy (id= "button1")
+    private AndroidElement allow_consent;
+
+    @AndroidFindBy(id="cbKycConsent")
+    private AndroidElement kyc_consent;
+
+    @AndroidFindBy(id="btn_other_options")
+    private AndroidElement skip_other_option_cta;
+
+    @AndroidFindBy(id="btn_exit")
+    private AndroidElement skip_i_dont_want_benifits;
+
+    @AndroidFindBy(id="non_aadhaar_options")
+    private AndroidElement non_aadhaar_options;
+
+    @AndroidFindBy(id="cashback_text")
+    private AndroidElement upi_page_cashback_text;
+
+    @AndroidFindBy(id="icon_close")
+    private AndroidElement upi_page_cross;
+
+    @AndroidFindBy(id="title")
+    private AndroidElement email_screen_title;
+
+    @AndroidFindBy(id= "continue_button")
+    private AndroidElement cta_continue_button_email_screen;
+
+
 
 
     public OnboardingPage(AndroidDriver driver) {
@@ -60,6 +107,70 @@ public class OnboardingPage {
     public HomePage clickOnSkip() throws IOException {
         Element.selectElement(driver, button_skip, "Click on Skip button");
         return new HomePage(driver);
+    }
+
+    public void enterOtp(String otp) throws InterruptedException {
+        Element.enterText(driver, textbox_enter_otp, otp, "OTP");
+    }
+
+    public void clickSubmitOtpCta() {
+        Element.selectElement(driver, cta_submit_otp, "Submit OTP CTA");
+    }
+
+    public String getPermissionText(){
+        return Element.getText(driver, permission_statement, "Get Permission Text");
+    }
+
+    public void clickContinueWithAadhaarCta(){
+        Element.selectElement(driver, cta_continue_with_aadhaar, "Continue With Aadhaar");
+    }
+
+    public String getConsentPopUpTitleText(){
+        return Element.getText(driver, permission_consent, "Get Consent Title Text");
+    }
+
+    public void clickAllowConsent(){
+        Element.selectElement(driver, allow_consent, "Allow Consent");
+    }
+
+    public String getKycConsentText(){
+        return Element.getText(driver, kyc_consent, "Get Kyc Consent Text");
+    }
+
+    public void clickOnKYCSkipCta(){
+        Element.selectElement(driver, cta_skip, "Skip KYc");
+    }
+
+    public void clickOnOtherKycOptionCta(){
+        Element.selectElement(driver, skip_other_option_cta, "Choose Other KYC option");
+    }
+
+    public boolean isNonAadhaarOptionsAvailable() throws InterruptedException {
+        if (Element.isElementPresent(driver, By.id("non_aadhaar_options"))){
+            return true;
+        }else {
+        return false;
+        }
+    }
+
+    public void clickOnIdontWantBenifitsCta(){
+        Element.selectElement(driver, skip_i_dont_want_benifits, "Choose I Dont Want Benifits option");
+    }
+
+    public String getUpiPageCashbackText(){
+        return Element.getText(driver, upi_page_cashback_text, "Get Upi Page Cashback Text");
+    }
+
+    public void clickOnUpiPageCross(){
+        Element.selectElement(driver, upi_page_cross, "Close Upi Page");
+    }
+
+    public String getEmailScreenText(){
+        return Element.getText(driver, email_screen_title, "Get Email Page Title Text");
+    }
+
+    public void clickOnContinueCta(){
+        Element.selectElement(driver, cta_continue_button_email_screen, "Click on continue Button");
     }
 
 }
