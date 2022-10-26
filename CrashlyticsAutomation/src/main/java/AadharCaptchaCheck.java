0ab9014b13432076fd64e999b1d58c1a413fd462
uditgupta <udit.gupta@mobikwik.com>
2022-10-26 14:27:10 +0530

    close browser on exception

--- CrashlyticsAutomation/src/main/java/AadharCaptchaCheck.java
+++ CrashlyticsAutomation/src/main/java/AadharCaptchaCheck.java
@@ -22,101 +22,114 @@ public class AadharCaptchaCheck {
     public static void aadharCheck () throws IOException, InterruptedException {
         String username = "mbkmobile.team@mobikwik.com";
         String pass = "Mobikwik@123456",data="";
-        Boolean aadharText,captchaText,captchaImage=false ;
+        Boolean aadharText=false,captchaText=false,captchaImage=false;
+        int refreshFlag=0;
+        WebDriver driver;
 
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
-        WebDriver driver=new ChromeDriver();
-        driver.get("https://resident.uidai.gov.in/offline-kyc");
-        Thread.sleep(5000);
-        SoftAssert ar= new SoftAssert();
-        ar.assertEquals(driver.findElement(By.xpath("//label[@data-original-title='Aadhaar Number']")).getText(),"Aadhaar Number","Aadhar card text check");
-        ar.assertEquals(driver.findElement(By.xpath("//label[@data-original-title='Captcha Verification']")).getText(),"Captcha Verification","Captcha text check");
-        ar.assertTrue(driver.findElement(By.xpath("//img[@id='captcha-img']")).isDisplayed(),"Captcha image check");
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
+            driver = new ChromeDriver();
+        try {
+            driver.get("https://resident.uidai.gov.in/offline-kyc");
+            while (refreshFlag < 2) {
+                Thread.sleep(5000);
+                SoftAssert ar = new SoftAssert();
+                ar.assertEquals(driver.findElement(By.xpath("//label[@data-original-title='Aadhaar Number']")).getText(), "Aadhaar Number", "Aadhar card text check");
+                ar.assertEquals(driver.findElement(By.xpath("//label[@data-original-title='Captcha Verification']")).getText(), "Captcha Verification", "Captcha text check");
+                ar.assertTrue(driver.findElement(By.xpath("//img[@id='captcha-img']")).isDisplayed(), "Captcha image check");
 
-        aadharText=driver.findElement(By.xpath("//label[@data-original-title='Aadhaar Number']")).isDisplayed();
-        captchaText=driver.findElement(By.xpath("//label[@data-original-title='Captcha Verification']")).isDisplayed();
+                aadharText = driver.findElement(By.xpath("//label[@data-original-title='Aadhaar Number']")).isDisplayed();
+                captchaText = driver.findElement(By.xpath("//label[@data-original-title='Captcha Verification']")).isDisplayed();
 
-        if(driver.findElement(By.xpath("//img[@id='captcha-img']")).isDisplayed()){
-            File src= ((TakesScreenshot)driver.findElement(By.xpath("//img[@id='captcha-img']"))).getScreenshotAs(OutputType.FILE);
-            File dest=new File(System.getProperty("user.dir") + "/Test1.jpg");
-            FileUtils.copyFile(src,dest);
-            Ocr ocr = new Ocr();
+                if (driver.findElement(By.xpath("//img[@id='captcha-img']")).isDisplayed()) {
+                    File src = ((TakesScreenshot) driver.findElement(By.xpath("//img[@id='captcha-img']"))).getScreenshotAs(OutputType.FILE);
+                    File dest = new File(System.getProperty("user.dir") + "/Test1.jpg");
+                    FileUtils.copyFile(src, dest);
+                    Ocr ocr = new Ocr();
 
-            ocr.startEngine("eng", Ocr.SPEED_FASTEST);
+                    ocr.startEngine("eng", Ocr.SPEED_FASTEST);
 
-            String getText = ocr.recognize(new File[] { new File(System.getProperty("user.dir") + "/Test1.jpg") },
-                    Ocr.RECOGNIZE_TYPE_TEXT, Ocr.OUTPUT_FORMAT_PLAINTEXT);
+                    String getText = ocr.recognize(new File[]{new File(System.getProperty("user.dir") + "/Test1.jpg")},
+                            Ocr.RECOGNIZE_TYPE_TEXT, Ocr.OUTPUT_FORMAT_PLAINTEXT);
 
-            ocr.stopEngine();
+                    ocr.stopEngine();
 
-            if(!getText.isEmpty()){
-                captchaImage=true;
-            }
-
-            System.out.println(getText);
-
-        }
-
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
-        Session session = Session.getDefaultInstance(props,
-
-                new Authenticator() {
-
-                    protected PasswordAuthentication getPasswordAuthentication() {
-
-                        return new PasswordAuthentication(username, pass);
-
+                    if (!getText.isEmpty()) {
+                        captchaImage = true;
+                        break;
+                    } else {
+                        driver.findElement(By.id("refresh-captcha")).click();
+                        refreshFlag = refreshFlag + 1;
                     }
 
-                });
+                    System.out.println(getText);
+                }
 
-        try {
-            Message message = new MimeMessage(session);
+            }
+
+            Properties props = new Properties();
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.socketFactory.port", "465");
+            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", "465");
+            Session session = Session.getDefaultInstance(props,
+
+                    new Authenticator() {
+
+                        protected PasswordAuthentication getPasswordAuthentication() {
+
+                            return new PasswordAuthentication(username, pass);
+
+                        }
+
+                    });
+
+            try {
+                Message message = new MimeMessage(session);
 //            message.setFrom(new InternetAddress("qafront-end@mobikwik.com"));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("qafront-end@mobikwik.com"));
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("udit.gupta@mobikwik.com"));
 
-            message.setRecipients(Message.RecipientType.CC, InternetAddress.parse("vipul.behl@mobikwik.com"));
-            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                message.setRecipients(Message.RecipientType.CC, InternetAddress.parse("vipul.behl@mobikwik.com"));
+                message.setRecipients(Message.RecipientType.CC, InternetAddress.parse("aditya.upadhyay@mobikwik.com"));
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
-            if(captchaImage) {
-                message.setSubject("Aadhar captcha Check | Pass | " + timestamp);
-            }else{
-                message.setSubject("ALERT!!!!!!!!!!!!!Aadhar captcha Check | Fail | " + timestamp+"!!!!!!!!!!!!");
+                if (captchaImage) {
+                    message.setSubject("Aadhar captcha Check | Pass | " + timestamp);
+                } else {
+                    message.setSubject("ALERT!!!!!!!!!!!!!Aadhar captcha Check | Fail | " + timestamp + "!!!!!!!!!!!!");
+                }
+
+                if (aadharText) {
+                    data = data + "<b> Aadhar text is present <br>";
+                } else {
+                    data = data + "<b> Aadhar text is not present <br>";
+                }
+                if (captchaText) {
+                    data = data + " Captcha text is present <br>";
+                } else {
+                    data = data + " Captcha text is not present <br>";
+                }
+                if (captchaImage) {
+                    message.setContent(data + " Captcha is present", "text/html");
+                } else {
+                    message.setContent(data + " Captcha is not present", "text/html");
+                }
+
+
+                // finally send the email
+                Transport.send(message);
+
+                System.out.println("=====Email Sent=====");
+
+            } catch (MessagingException e) {
+
+                throw new RuntimeException(e);
+
             }
-
-            if(aadharText){
-                data=data+"<b> Aadhar text is present <br>";
-            }else{
-                data=data+"<b> Aadhar text is not present <br>";
-            }
-            if(captchaText){
-                data=data+" Captcha text is present <br>";
-            }else{
-                data=data+" Captcha text is not present <br>";
-            }
-            if(captchaImage) {
-                message.setContent(data +" Captcha is present","text/html");
-            }else {
-                message.setContent(data +" Captcha is not present","text/html");
-            }
-
-
-            // finally send the email
-            Transport.send(message);
-
-            System.out.println("=====Email Sent=====");
-
-        } catch (MessagingException e) {
-
-            throw new RuntimeException(e);
-
+            driver.close();
+        }catch (Exception e){
+            driver.close();
         }
-        driver.close();
     }
 
     }
