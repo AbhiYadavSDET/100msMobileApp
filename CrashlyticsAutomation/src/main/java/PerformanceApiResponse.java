f3bfde675f5b569e9b0edf428d265ad01ae21a87
Mayank Suneja <mayank.suneja@mobikwik.com>
2022-12-07 14:16:55 +0530

    Mailer changes to reenabling the Emails

--- CrashlyticsAutomation/src/main/java/PerformanceApiResponse.java
+++ CrashlyticsAutomation/src/main/java/PerformanceApiResponse.java
@@ -23,14 +23,15 @@ public class PerformanceApiResponse {
     @Test(groups = {"performanceAPIResp"}, priority = 0, description = "Verify api reponse time")
     public static void PerformanceApiResponse() throws InterruptedException, IOException {
         String username = "mbkmobile.team@mobikwik.com";
-        String pass = "Mobikwik@123456";
+        //String pass = "Mobikwik@123456";
+        String pass = "jcrqzdelrdyrzpua";
         String version = "22.39.5";
         String date = "60 m";
         String crashFreeUsers;
         int MAX_RETRIES = 2;
         int total_fresh = 0, total_topFive;
         String respTime, changePer, name, success, sample, changeSuccess;
-        float respTimeCheck,successCheck;
+        float respTimeCheck, successCheck;
         int i, percent, pages = 1;
         float samplesCheck;
         Boolean condition = true, sendMail = false, changeSub = true;
@@ -104,7 +105,6 @@ public class PerformanceApiResponse {
 //        driver.findElement(By.xpath("(//span[@class='header-text-wrapper ng-star-inserted'])[2]")).click();
 
 
-
             while (condition && pages < 4) {
                 longWait(driver, "(//mat-cell[@class='mat-cell cdk-cell metric-value-cell cdk-column-RESPONSE_TIME_LATEST_VALUE mat-column-RESPONSE_TIME_LATEST_VALUE ng-star-inserted'])[1]");
                 Thread.sleep(4000);
@@ -172,17 +172,17 @@ public class PerformanceApiResponse {
             driver.findElement(By.xpath("//span[contains(text(),'24 hour')]")).click();
 
             js.executeScript("window.scrollBy(0,150)", "");
-            WebDriverWait wait=new WebDriverWait(driver,10);
+            WebDriverWait wait = new WebDriverWait(driver, 10);
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='header-text-wrapper ng-star-inserted']")));
-            shortWait(driver,"[//mat-cell(@class='mat-cell cdk-cell metric-delta-cell standard cdk-column-SUCCESS_RATE_DELTA mat-column-SUCCESS_RATE_DELTA ng-star-inserted'])[1]");
+            shortWait(driver, "[//mat-cell(@class='mat-cell cdk-cell metric-delta-cell standard cdk-column-SUCCESS_RATE_DELTA mat-column-SUCCESS_RATE_DELTA ng-star-inserted'])[1]");
             driver.findElement(By.xpath("(//span[@class='header-text-wrapper ng-star-inserted'])[5]")).click();
 
-            condition=true;
-            pages=1;
-            while(condition && pages<4){
-                longWait(driver,"(//mat-cell[@class='mat-cell cdk-cell metric-delta-cell standard cdk-column-SUCCESS_RATE_DELTA mat-column-SUCCESS_RATE_DELTA ng-star-inserted'])[1]");
+            condition = true;
+            pages = 1;
+            while (condition && pages < 4) {
+                longWait(driver, "(//mat-cell[@class='mat-cell cdk-cell metric-delta-cell standard cdk-column-SUCCESS_RATE_DELTA mat-column-SUCCESS_RATE_DELTA ng-star-inserted'])[1]");
                 Thread.sleep(4000);
-                for(i=1;i<=10;i++){
+                for (i = 1; i <= 10; i++) {
                     respTime = driver.findElement(By.xpath("(//mat-cell[@class='mat-cell cdk-cell metric-value-cell cdk-column-RESPONSE_TIME_LATEST_VALUE mat-column-RESPONSE_TIME_LATEST_VALUE ng-star-inserted'])[" + i + "]")).getText();
 //                    respTimeCheck = Float.parseFloat(respTime.replace("s", "").replace("min", ""));
                     shortWait(driver, "//mat-cell[@class='mat-cell cdk-cell metric-delta-cell extended cdk-column-RESPONSE_TIME_DELTA mat-column-RESPONSE_TIME_DELTA ng-star-inserted']");
@@ -195,15 +195,15 @@ public class PerformanceApiResponse {
 //                System.out.println(driver.findElement(By.xpath("(//a[@class='mat-mdc-tooltip-trigger fire-router-link-host data-text-wrapper'])["+i+"]")).getText());
                     shortWait(driver, "//mat-cell[@class='mat-cell cdk-cell metric-value-cell cdk-column-SUCCESS_RATE_LATEST_VALUE mat-column-SUCCESS_RATE_LATEST_VALUE ng-star-inserted']");
                     success = driver.findElement(By.xpath("(//mat-cell[@class='mat-cell cdk-cell metric-value-cell cdk-column-SUCCESS_RATE_LATEST_VALUE mat-column-SUCCESS_RATE_LATEST_VALUE ng-star-inserted'])[" + i + "]")).getText();
-                    successCheck = Float.parseFloat(success.replace("%",""));
+                    successCheck = Float.parseFloat(success.replace("%", ""));
 //                Thread.sleep(2000);
                     shortWait(driver, "//div[@class='resource-subtitle ng-star-inserted']");
                     sample = driver.findElement(By.xpath("(//div[@class='resource-subtitle ng-star-inserted']/p)[" + i + "]")).getText();
                     samplesCheck = Float.parseFloat(sample.replace(" samples", "").replace("K", "").replace("M", ""));
 
-                    changeSuccess=driver.findElement(By.xpath("(//mat-cell[@class='mat-cell cdk-cell metric-delta-cell standard cdk-column-SUCCESS_RATE_DELTA mat-column-SUCCESS_RATE_DELTA ng-star-inserted'])["+ i +"]")).getText();
+                    changeSuccess = driver.findElement(By.xpath("(//mat-cell[@class='mat-cell cdk-cell metric-delta-cell standard cdk-column-SUCCESS_RATE_DELTA mat-column-SUCCESS_RATE_DELTA ng-star-inserted'])[" + i + "]")).getText();
 
-                    if ( (sample.contains("K") || samplesCheck > 30 || sample.contains("M")) && !name.startsWith("static") && successCheck<90) {
+                    if ((sample.contains("K") || samplesCheck > 30 || sample.contains("M")) && !name.startsWith("static") && successCheck < 90) {
                         if (changeSuccess.contains("-") || changeSuccess.contains(">")) {
                             responseTime1.add(respTime);
                             changePercent1.add(changePer);
@@ -231,7 +231,7 @@ public class PerformanceApiResponse {
                 js.executeScript("window.scrollBy(0,100)", "");
             }
             for (i = 0; i < changePercent1.size(); i++) {
-                System.out.println(responseTime1.get(i) + "   " + changePercent1.get(i) + "   " + apiName1.get(i) + "   " + apiSuccess1.get(i) + "   " + apiSample1.get(i) + " "+changeSuccess1.get(i));
+                System.out.println(responseTime1.get(i) + "   " + changePercent1.get(i) + "   " + apiName1.get(i) + "   " + apiSuccess1.get(i) + "   " + apiSample1.get(i) + " " + changeSuccess1.get(i));
             }
             if (changeSuccess1.size() >= 0) {
                 sendMail = true;
@@ -246,31 +246,31 @@ public class PerformanceApiResponse {
 //        }catch (Exception e) {
 //            driver.close();
 //        }
-        }catch (Exception e){
+        } catch (Exception e) {
 //            throw e;
             driver.quit();
         }
 
-        if(sendMail){
-        String m;
-        int n;
-        n=changePercent.size();
-        String test = "", testRed = "";
+        if (sendMail) {
+            String m;
+            int n;
+            n = changePercent.size();
+            String test = "", testRed = "";
 //        testRed = "<H2 style=\"background-color:red;\">Top 5 Crash (URGENT CHECK)</H2><table cellpadding=\"8px\" cellspacing=\"1\" border=\"1\" style=\"width:60%;text-align: centre;\">" + "<tr><th>Serial No.</th><th>Title</th><th>Subtitle</th><th>URL</th><th>Version</th><th>Events</th><th>Users</th></tr>";
-        test = "<H2>API Performance</H2><H3>Response change</H3><table cellpadding=\"8px\" cellspacing=\"1\" border=\"1\" style=\"width:60%;text-align: centre;\"><tr><th>Serial No.</th><th>Api Name</th><th>Sample</th><th>Response Time</th><th>Response percent change</th><th>Success</th></tr>";
-        m = "</table>";
-        for ( i = 1; i <= n; i++) {
-            test = test + "<td>" + i + "</td><td>" + apiName.get(i - 1) + "</td><td>" + apiSample.get(i - 1) + "</td><td>" + responseTime.get(i-1) + "</td><td>" + changePercent.get(i - 1) + "</td><td>" + apiSuccess.get(i - 1) + "</td><tr></tr>";
-        }
-        if (n == 0) {
-            test = test + "<td> No issues </td>";
-        }
-        test = test + m;
-        n= changePercent1.size();
+            test = "<H2>API Performance</H2><H3>Response change</H3><table cellpadding=\"8px\" cellspacing=\"1\" border=\"1\" style=\"width:60%;text-align: centre;\"><tr><th>Serial No.</th><th>Api Name</th><th>Sample</th><th>Response Time</th><th>Response percent change</th><th>Success</th></tr>";
+            m = "</table>";
+            for (i = 1; i <= n; i++) {
+                test = test + "<td>" + i + "</td><td>" + apiName.get(i - 1) + "</td><td>" + apiSample.get(i - 1) + "</td><td>" + responseTime.get(i - 1) + "</td><td>" + changePercent.get(i - 1) + "</td><td>" + apiSuccess.get(i - 1) + "</td><tr></tr>";
+            }
+            if (n == 0) {
+                test = test + "<td> No issues </td>";
+            }
+            test = test + m;
+            n = changePercent1.size();
 
             test = test + "<H3>Success change</H3><table cellpadding=\"8px\" cellspacing=\"1\" border=\"1\" style=\"width:60%;text-align: centre;\"><tr><th>Serial No.</th><th>Api Name</th><th>Sample</th><th>Response Time</th><th>Response percent change</th><th>Success</th><th>Success Change</th></tr>";
-            for ( i = 1; i <= n; i++) {
-                test = test + "<td>" + i + "</td><td>" + apiName1.get(i - 1) + "</td><td>" + apiSample1.get(i - 1) + "</td><td>" + responseTime1.get(i-1) + "</td><td>" + changePercent1.get(i - 1) + "</td><td>" + apiSuccess1.get(i - 1) + "</td><td>" + changeSuccess1.get(i - 1) + "</td><tr></tr>";
+            for (i = 1; i <= n; i++) {
+                test = test + "<td>" + i + "</td><td>" + apiName1.get(i - 1) + "</td><td>" + apiSample1.get(i - 1) + "</td><td>" + responseTime1.get(i - 1) + "</td><td>" + changePercent1.get(i - 1) + "</td><td>" + apiSuccess1.get(i - 1) + "</td><td>" + changeSuccess1.get(i - 1) + "</td><tr></tr>";
             }
             if (n == 0) {
                 test = test + "<td> No issues </td>";
@@ -282,8 +282,10 @@ public class PerformanceApiResponse {
             // this will set host of server- you can change based on your requirement
             props.put("mail.smtp.host", "smtp.gmail.com");
 
+            props.put("mail.smtp.starttls.enable", "true");
+
             // set the port of socket factory
-            props.put("mail.smtp.socketFactory.port", "465");
+            props.put("mail.smtp.socketFactory.port", "587");
 
             // set socket factory
             props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
@@ -292,7 +294,7 @@ public class PerformanceApiResponse {
             props.put("mail.smtp.auth", "true");
 
             // set the port of SMTP server
-            props.put("mail.smtp.port", "465");
+            props.put("mail.smtp.port", "587");
 
             // This will handle the complete authentication
             Session session = Session.getDefaultInstance(props,
@@ -329,17 +331,17 @@ public class PerformanceApiResponse {
                 // Add the subject link
 //                Timestamp timestamp = new Timestamp(System.currentTimeMillis()); //with date and time
 //            System.out.println(timestamp);
-                if(changePercent.size()>0) {
+                if (changePercent.size() > 0) {
                     if (changeSub) {
                         message.setSubject("Api Performance | Report | " + java.time.LocalDate.now()); // with only date
                     } else {
                         message.setSubject("!!!!!!!!!!!!!!!!!!!!ALERT!!!!!!!!!!!!!!!!!!!!!!!   Api Performance | Report | " + java.time.LocalDate.now());
                     }
-                }else{
+                } else {
                     message.setSubject(":::::::::::::::::::::::Everything Looks good:::::::::::::::::::::: Api Performance | Report | " + java.time.LocalDate.now());
                 }
 
-                message.setContent(  test , "text/html");
+                message.setContent(test, "text/html");
 
 
                 // finally send the email
@@ -357,28 +359,28 @@ public class PerformanceApiResponse {
 
     }
 
-    public static void shortWait(WebDriver driver,String text){
+    public static void shortWait(WebDriver driver, String text) {
         try {
             WebDriverWait wait = new WebDriverWait(driver, 10);
             WebElement element;
             element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(text)));
-        } catch (Exception e){
-            if(!e.toString().contains("TimeoutException")){
+        } catch (Exception e) {
+            if (!e.toString().contains("TimeoutException")) {
                 Assert.assertTrue(false);
             }
         }
     }
 
-    public static void longWait(WebDriver driver,String text){
-        try{
-        WebDriverWait wait=new WebDriverWait(driver, 40);
-        WebElement element;
-        element= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(text)));
-    } catch (Exception e){
-        if(!e.toString().contains("TimeoutException")){
-            Assert.assertTrue(false);
+    public static void longWait(WebDriver driver, String text) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, 40);
+            WebElement element;
+            element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(text)));
+        } catch (Exception e) {
+            if (!e.toString().contains("TimeoutException")) {
+                Assert.assertTrue(false);
+            }
         }
-    }
     }
 
 }
