e217d7235aefdbbacdb360ee5c3180043ce6acaf
Mayank Suneja <mayank.suneja@mobikwik.com>
2023-01-09 19:48:26 +0530

    Added the small changes in MF TC

--- Appium/src/main/java/Helpers/MutualFundsHelper.java
+++ Appium/src/main/java/Helpers/MutualFundsHelper.java
@@ -1,13 +1,13 @@
 package Helpers;
 
+import Logger.Log;
 import PageObject.HomePage;
 import PageObject.MutualFundPage;
-import Utils.MBReporter;
-import io.appium.java_client.android.AndroidDriver;
-import Logger.Log;
-import org.openqa.selenium.By;
 import Utils.Element;
+import Utils.MBReporter;
 import Utils.Screen;
+import io.appium.java_client.android.AndroidDriver;
+
 import java.io.IOException;
 
 
@@ -32,7 +32,7 @@ public class MutualFundsHelper {
 
     }
 
-    public void mutualFundsCheck() throws InterruptedException, IOException {
+    public void mutualFundsCheck(String fundName, String minInvestAmount, String mfReturns, String mfRating, String expPopUpTitle, String expPopUpSubTitle, String expPopUpCtaText) throws InterruptedException, IOException {
 
         Log.info("START", "Explore Mutual Funds");
 
@@ -66,13 +66,33 @@ public class MutualFundsHelper {
         Log.info("Mutual Fund Returns : " + returns);
         Log.info("Mutual Fund Rating : " + rating);
 
+        // Add the assertions
+        mbReporter.verifyEquals(mutualFundName, fundName, "Verify MF Name", false, false);
+        mbReporter.verifyEquals(minimumInvestmentAmount, minInvestAmount, "Verify MF Min Amount", false, false);
+        mbReporter.verifyEquals(returns, mfReturns, "Verify MF Returns", false, false);
+        mbReporter.verifyEquals(rating, mfRating, "Verify MF rating", false, false);
+
         // Selecting the mutual fund.
         mutualFundPage.selectMutualFund();
 
-        Thread.sleep(5000);
-
         // Click on sip.
         mutualFundPage.clickOnSip();
+
+        // Getting mutual fund details.
+        String popupTitle = mutualFundPage.getPopUpTitle();
+        String popupSubTitle = mutualFundPage.getPopUpSubTitle();
+        String popCtaText = mutualFundPage.getPopUpCtaText();
+
+        // Printing mutual fund details.
+        Log.info("Popup Title : " + popupTitle);
+        Log.info("Popup SubTitle : " + popupSubTitle);
+        Log.info("Popup Cta text : " + popCtaText);
+
+        // Add the assertions
+        mbReporter.verifyEquals(popupTitle, expPopUpTitle, "Verify Pop Up Title", false, false);
+        mbReporter.verifyEquals(popupSubTitle, expPopUpSubTitle, "Verify Pop Up Sub Title", false, false);
+        mbReporter.verifyEquals(popCtaText, expPopUpCtaText, "Verify Pop Up Cta Text", false, false);
+
 
         Log.info("END", "Explore Mutual Funds");
 
