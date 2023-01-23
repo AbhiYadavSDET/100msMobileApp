d56c0a35adb0c4d526cbccc1ae38bf4766b36dfb
Mayank Suneja <mayank.suneja@mobikwik.com>
2023-01-23 17:23:51 +0530

    Added the assertions on the p2p flow

--- Appium/src/main/java/Helpers/P2MHelper.java
+++ Appium/src/main/java/Helpers/P2MHelper.java
@@ -12,6 +12,7 @@ import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.support.PageFactory;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
 
 public class P2MHelper {
 
@@ -21,6 +22,9 @@ public class P2MHelper {
     P2MPage p2mPage;
     Screen screen;
     MBReporter mbReporter;
+    LinkedHashMap<String, String> balanceBefore;
+    LinkedHashMap<String, String> balanceAfter;
+    MBKCommonControlsHelper mbkCommonControlsHelper;
 
 
     public P2MHelper(AndroidDriver driver) throws IOException {
@@ -30,10 +34,15 @@ public class P2MHelper {
         p2mPage = new P2MPage(driver);
         screen = new Screen(driver);
         mbReporter = new MBReporter(driver);
+        mbkCommonControlsHelper = new MBKCommonControlsHelper(driver);
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
     public void p2mSend(String merchantCode, String amount, String expStatus, String expAmount, String expReceiverName, String expMerchantName, String expMerchantCode, String expZipCtaText) throws InterruptedException, IOException {
+
+        // Get the Balance if the User Before TRX
+        balanceBefore = mbkCommonControlsHelper.getBalance();
+
 
         // Tap the QR code Icon on Homepage
         p2mPage.clickScanQR();
@@ -87,9 +96,36 @@ public class P2MHelper {
 
 
         // Click on the up Icon
+        p2mPage.clickUpButton();
+
+        // Click Cross Buttonm
+        p2mPage.clickBackButton();
+
+        // Click on the up Icon
+        p2mPage.clickUpButton();
+
+        // Click on the back button if the bottom sheet is present
+        Thread.sleep(3000);
+        if (Elements.isElementPresent(driver, p2mPage.upiBottomSheetCta)) {
+            Elements.back(driver, "Navigate Back");
+        }
+
+        // Get the Balance if the User Before TRX
+        balanceAfter = mbkCommonControlsHelper.getBalance();
 
 
-        // Click on Home
+        // Assertions on the balance deducted
+        Double actualMainBalanceAfter = Double.parseDouble(mbkCommonControlsHelper.getBalance(balanceAfter, MBKCommonControlsHelper.BalanceType.MAINBALANCE)) * 100;
+        Double actualMoneyAddedAfter = Double.parseDouble(mbkCommonControlsHelper.getBalance(balanceAfter, MBKCommonControlsHelper.BalanceType.MONEYADDED)) * 100;
+        Double actualSupercashAfter = Double.parseDouble(mbkCommonControlsHelper.getBalance(balanceAfter, MBKCommonControlsHelper.BalanceType.SUPERCASH)) * 100;
+
+        Double expectedMainBalanceAfter = Double.parseDouble(mbkCommonControlsHelper.getBalance(balanceBefore, MBKCommonControlsHelper.BalanceType.MAINBALANCE)) * 100 - Double.parseDouble(amount) * 100;
+        Double expectedMoneyAddedAfter = Double.parseDouble(mbkCommonControlsHelper.getBalance(balanceBefore, MBKCommonControlsHelper.BalanceType.MONEYADDED)) * 100 - Double.parseDouble(amount) * 100;
+        Double expectedSupercashAfter = Double.parseDouble(mbkCommonControlsHelper.getBalance(balanceBefore, MBKCommonControlsHelper.BalanceType.SUPERCASH)) * 100;
+
+        mbReporter.verifyEquals(actualMainBalanceAfter, expectedMainBalanceAfter, "Post TRX | Verify Wallet Main Balance", false, false);
+        mbReporter.verifyEquals(actualMoneyAddedAfter, expectedMoneyAddedAfter, "Post TRX | Verify Money Added Balance", false, false);
+        mbReporter.verifyEquals(actualSupercashAfter, expectedSupercashAfter, "Post TRX | Verify Supercash Balance", false, false);
 
 
     }
