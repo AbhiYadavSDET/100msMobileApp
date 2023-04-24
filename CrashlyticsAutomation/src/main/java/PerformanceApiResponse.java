1f56ef0e9b7bbd10124a8569b26991783b423b12
Paraj Jain <paraj.jain@mobikwik.com>
2023-04-24 17:17:25 +0530

    Fixed Success Change and Added Logging

--- CrashlyticsAutomation/src/main/java/PerformanceApiResponse.java
+++ CrashlyticsAutomation/src/main/java/PerformanceApiResponse.java
@@ -71,11 +71,14 @@ public class PerformanceApiResponse {
         /** Array List for Data */
         System.out.println("Array List for Data");
 
+        /** Array List for Response Change Tables */
         ArrayList<String> responseTime = new ArrayList<String>();
         ArrayList<String> changePercent = new ArrayList<String>();
         ArrayList<String> apiName = new ArrayList<String>();
         ArrayList<String> apiSuccess = new ArrayList<String>();
         ArrayList<String> apiSample = new ArrayList<String>();
+
+        /** Array List for Success Change Tables */
         ArrayList<String> responseTime1 = new ArrayList<String>();
         ArrayList<String> changePercent1 = new ArrayList<String>();
         ArrayList<String> apiName1 = new ArrayList<String>();
@@ -84,37 +87,35 @@ public class PerformanceApiResponse {
         ArrayList<String> changeSuccess1 = new ArrayList<String>();
 
 
+/** Driver Initiate Method 1 : Chorme Browser Local */
 
-//        /** Initiating Chrome driver */
-//        System.out.println("Initiating Chrome driver");
-//
-//        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
-////        WebDriver driver = new ChromeDriver();
-//
-//
-////        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
-//        ChromeOptions chromeOptions = new ChromeOptions();
+        /** Initiating Chrome driver */
+        System.out.println("Initiating Chrome driver");
+
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
 //        chromeOptions.addArguments("--headless");
+
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+
+/** Driver Initiate Method 2 : Chorme Browser Browserstack */
+
+
+//        /** Initiating Browserstack Chrome driver */
+//        System.out.println("Initiating Browserstack Chrome driver");
 //
-//        WebDriver driver = new ChromeDriver(chromeOptions);
-
-//        LogEntries log= driver.manage().logs().get("browser");
-//        List<LogEntry> logs=log.getAll();
-
-
-
-        /** Initiating Browserstack Chrome driver */
-        System.out.println("Initiating Browserstack Chrome driver");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("os", "Windows");
-        capabilities.setCapability("os_version", "10");
-        capabilities.setCapability("browser", "chrome");
-        capabilities.setCapability("browser_version", "latest");
-        capabilities.setCapability("name", "MBKTest"); // test buildName
-//        capabilities.setCapability("build", buildName); // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
-
-        WebDriver driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("os", "Windows");
+//        capabilities.setCapability("os_version", "10");
+//        capabilities.setCapability("browser", "chrome");
+//        capabilities.setCapability("browser_version", "latest");
+//        capabilities.setCapability("name", "MBKTest"); // test buildName
+////        capabilities.setCapability("build", buildName); // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
 
 
 
@@ -183,8 +184,9 @@ public class PerformanceApiResponse {
             try {
                 System.out.println("Select Performance Main Title from Side Drawer");
                 driver.findElement(By.xpath("//div[text()='Performance']")).click();
+
             } catch (Exception e) {
-                System.out.println("Select Performance Backup under Release & Monitor from Side Drawer");
+                System.out.println(" EXCEPTION : Select Performance Backup under Release & Monitor from Side Drawer");
                 driver.findElement(By.xpath("//div[text()=' Release & Monitor ']")).click();
 
                 Thread.sleep(2000);
@@ -227,6 +229,8 @@ public class PerformanceApiResponse {
 
             System.out.println("Extracting Data and validating through conditions");
 
+
+            System.out.println("Initializing Page Locators");
             String responseTimeCell="(//mat-cell[@class='mat-cell cdk-cell metric-value-cell cdk-column-RESPONSE_TIME_LATEST_VALUE mat-column-RESPONSE_TIME_LATEST_VALUE ng-star-inserted'])";
             String responseTimeChange= "(//mat-cell[@class='mat-cell cdk-cell metric-delta-cell extended cdk-column-RESPONSE_TIME_DELTA mat-column-RESPONSE_TIME_DELTA ng-star-inserted'])";
             String networkApiName= "(//a[@class='mat-mdc-tooltip-trigger fire-router-link-host data-text-wrapper'])";
@@ -236,29 +240,46 @@ public class PerformanceApiResponse {
             String pageNavigation="//button[@aria-label='Next page']";
 
 
+            /** Response Change Table Process Begins */
+            System.out.println("Response Change Table Process Begins");
+
             while (condition && pages < 4) {
+                System.out.println("While Loop Starts for Multiple Pages");
+                System.out.println("Page Number : "+pages);
+                System.out.println("Condition TRUE/FALSE : " + condition);
                 longWait(driver, responseTimeCell);
                 Thread.sleep(4000);
+
+
                 for (i = 1; i <= 10; i++) {
 
+                    System.out.println("Extracting Response Time For Record Number : "+i+ " And on Page number : "+ pages);
                     respTime = driver.findElement(By.xpath(responseTimeCell+"[" + i + "]")).getText();
                     respTimeCheck = Float.parseFloat(respTime.replace("s", "").replace("min", ""));
+
+                    System.out.println("Extracting Change Percentage For Record Number : "+i+ " And on Page number : "+ pages);
                     shortWait(driver, responseTimeChange);
                     changePer = driver.findElement(By.xpath(responseTimeChange+"[" + i + "]")).getText();
                     percent = Integer.parseInt(changePer.replace("%", "").replace(",", "").replace("+", "").replace("-", "").replace(">", ""));
 
+                    System.out.println("Extracting Name For Record Number : "+i+ " And on Page number : "+ pages);
                     shortWait(driver, networkApiName);
                     name = driver.findElement(By.xpath(networkApiName+"[" + i + "]")).getText();
 
+                    System.out.println("Extracting Api Success Percentage For Record Number : "+i+ " And on Page number : "+ pages);
                     shortWait(driver, successPercentage);
                     success = driver.findElement(By.xpath(successPercentage+"[" + i + "]")).getText();
 
+                    System.out.println("Extracting API Sample Size For Record Number : "+i+ " And on Page number : "+ pages);
                     shortWait(driver, sampleSize);
                     sample = driver.findElement(By.xpath(sampleSize+"[" + i + "]")).getText();
                     samplesCheck = Float.parseFloat(sample.replace(" samples", "").replace("K", "").replace("M", ""));
 
                     if ((respTimeCheck > 10 || respTime.contains("min")) && percent > 100 && (sample.contains("K") || samplesCheck > 30 || sample.contains("M"))) {
                         if (changePer.contains("+") || changePer.contains(">")) {
+
+                            System.out.println("Adding All Response Change data for Record : "+i+ " For page : "+ pages+" to the respective Lists");
+
                             responseTime.add(respTime);
                             changePercent.add(changePer);
                             apiName.add(name);
@@ -273,17 +294,31 @@ public class PerformanceApiResponse {
 
                 try {
                     if (driver.findElement(By.xpath(pageNavigation)).isEnabled()) {
+                        System.out.println("Clicked > for Next page");
                         driver.findElement(By.xpath(pageNavigation)).click();
                         pages = pages + 1;
                         condition = true;
                         Thread.sleep(3000);
                     }
                 } catch (Exception e) {
+                    System.out.println("EXCEPTION : Clicking > for Next page for Response Change");
                     condition = false;
                 }
                 js.executeScript("window.scrollBy(0,100)", "");
             }
 
+            for (i = 0; i < responseTime.size(); i++) {
+                System.out.println(responseTime.get(i) + "   " + changePercent.get(i) + "   " + apiName.get(i) + "   " + apiSuccess.get(i) + "   " + apiSample.get(i) + " " + changeSuccess1.get(i));
+            }
+
+
+
+
+            /** Response Change Table Process Ends */
+            System.out.println("Response Change Table Process Ends");
+
+            /** Send Mail */
+            System.out.println("Send Mail Config Setting True/False based on condition");
             if (changePercent.size() >= 0) {
                 sendMail = true;
                 if (changePercent.size() > 5) {
@@ -292,47 +327,68 @@ public class PerformanceApiResponse {
             }
 
 
+            /** Success Change Table Process Begins */
+            System.out.println("Success Change Table Process Begins");
+
             driver.navigate().refresh();
             Thread.sleep(2000);
             longWait(driver, "//div[@class='date-selection']");
+            System.out.println("Open Date Selector");
             driver.findElement(By.xpath("//div[@class='date-selection']")).click();
             Thread.sleep(2000);
+            System.out.println("Select 24 hrs Data");
             driver.findElement(By.xpath("//span[contains(text(),'24 hour')]")).click();
 
             js.executeScript("window.scrollBy(0,150)", "");
             WebDriverWait wait = new WebDriverWait(driver, 10);
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='header-text-wrapper ng-star-inserted']")));
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='header-text-wrapper fire-popover-trigger ng-star-inserted']")));
             shortWait(driver, hourlyChange+"[1]");
-            driver.findElement(By.xpath("(//span[@class='header-text-wrapper ng-star-inserted'])[5]")).click();
+            System.out.println("Sort Success Change Ascending");
+            driver.findElement(By.xpath("//span[@class='header-text-wrapper fire-popover-trigger ng-star-inserted']")).click();
 
             condition = true;
             pages = 1;
             while (condition && pages < 4) {
+                System.out.println("While Loop Starts for Multiple Pages");
+                System.out.println("Page Number : "+pages);
+                System.out.println("Condition TRUE/FALSE : " + condition);
                 longWait(driver, hourlyChange+"[1]");
                 Thread.sleep(4000);
+
                 for (i = 1; i <= 10; i++) {
+                    System.out.println("Extracting Response Time For Record Number : "+i+ " And on Page number : "+ pages);
                     respTime = driver.findElement(By.xpath(responseTimeCell+"[" + i + "]")).getText();
 //                    respTimeCheck = Float.parseFloat(respTime.replace("s", "").replace("min", ""));
                     shortWait(driver, responseTimeChange);
+
+                    System.out.println("Extracting Change Percentage For Record Number : "+i+ " And on Page number : "+ pages);
                     changePer = driver.findElement(By.xpath(responseTimeChange+"[" + i + "]")).getText();
 //                    percent = Integer.parseInt(changePer.replace("%", "").replace(",", "").replace("+", "").replace("-", "").replace(">", ""));
-
                     shortWait(driver, networkApiName);
+
+                    System.out.println("Extracting Name For Record Number : "+i+ " And on Page number : "+ pages);
                     name = driver.findElement(By.xpath(networkApiName+"[" + i + "]")).getText();
 //                Thread.sleep(1000);
 //                System.out.println(driver.findElement(By.xpath(networkApiName+"["+i+"]")).getText());
                     shortWait(driver, successPercentage);
+
+                    System.out.println("Extracting Api Success Percentage For Record Number : "+i+ " And on Page number : "+ pages);
                     success = driver.findElement(By.xpath(successPercentage+"[" + i + "]")).getText();
                     successCheck = Float.parseFloat(success.replace("%", ""));
 //                Thread.sleep(2000);
                     shortWait(driver, sampleSize);
+
+                    System.out.println("Extracting API Sample Size For Record Number : "+i+ " And on Page number : "+ pages);
                     sample = driver.findElement(By.xpath(sampleSize+"[" + i + "]")).getText();
                     samplesCheck = Float.parseFloat(sample.replace(" samples", "").replace("K", "").replace("M", ""));
 
+                    System.out.println("Extracting 24 Hour change For Record Number : "+i+ " And on Page number : "+ pages);
                     changeSuccess = driver.findElement(By.xpath(hourlyChange+"[" + i + "]")).getText();
 
                     if ((sample.contains("K") || samplesCheck > 30 || sample.contains("M")) && !name.startsWith("static") && successCheck < 90) {
                         if (changeSuccess.contains("-") || changeSuccess.contains(">")) {
+
+                            System.out.println("Adding All Success Change data for Record : "+i+ " For page : "+ pages+" to the respective Lists");
                             responseTime1.add(respTime);
                             changePercent1.add(changePer);
                             apiName1.add(name);
@@ -348,19 +404,26 @@ public class PerformanceApiResponse {
                 }
                 try {
                     if (driver.findElement(By.xpath(pageNavigation)).isEnabled()) {
+                        System.out.println("Clicking > for Next page");
                         driver.findElement(By.xpath(pageNavigation)).click();
                         pages = pages + 1;
                         condition = true;
                         Thread.sleep(3000);
                     }
                 } catch (Exception e) {
+                    System.out.println("EXCEPTION : Clicking > for Next page for Success Change for Success Change");
                     condition = false;
                 }
                 js.executeScript("window.scrollBy(0,100)", "");
             }
+
             for (i = 0; i < changePercent1.size(); i++) {
                 System.out.println(responseTime1.get(i) + "   " + changePercent1.get(i) + "   " + apiName1.get(i) + "   " + apiSuccess1.get(i) + "   " + apiSample1.get(i) + " " + changeSuccess1.get(i));
             }
+
+
+            /** Send Mail */
+            System.out.println("Send Mail Config Setting True/False based on condition");
             if (changeSuccess1.size() >= 0) {
                 sendMail = true;
                 if (changeSuccess1.size() > 5) {
@@ -369,18 +432,20 @@ public class PerformanceApiResponse {
             }
 
 
-//        try{
+
             driver.quit();
-//        }catch (Exception e) {
-//            driver.close();
-//        }
+
         } catch (Exception e) {
 
+            System.out.println("EXCEPTION : Generic Exception Caught in main Code");
             driver.quit();
-//            throw e;
+
         }
 
+        /** Send Mail */
+
         if (sendMail) {
+            System.out.println("Send Mail Method Initiated");
             String m;
             int n;
             n = changePercent.size();
@@ -477,7 +542,7 @@ public class PerformanceApiResponse {
                 System.out.println("=====Email Sent=====");
 
             } catch (MessagingException e) {
-
+                System.out.println("EXCEPTION : While Sending Mail");
                 throw new RuntimeException(e);
 
             }
@@ -485,6 +550,7 @@ public class PerformanceApiResponse {
 
             /**Sending Suite Failure Mail*/
 
+            System.out.println("Send Mail: Suite Failure Method Initiated");
 
             Properties props = new Properties();
 
@@ -553,6 +619,7 @@ public class PerformanceApiResponse {
 
             } catch (MessagingException e) {
 
+                System.out.println("EXCEPTION : While Sending Failure Suite Mail");
                 throw new RuntimeException(e);
 
             }
@@ -571,6 +638,7 @@ public class PerformanceApiResponse {
             WebElement element;
             element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(text)));
         } catch (Exception e) {
+            System.out.println("EXCEPTION : Caught in Short Wait");
             if (!e.toString().contains("TimeoutException")) {
                 Assert.assertTrue(false);
             }
@@ -583,6 +651,8 @@ public class PerformanceApiResponse {
             WebElement element;
             element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(text)));
         } catch (Exception e) {
+
+            System.out.println("EXCEPTION : Caught in Long Wait");
             if (!e.toString().contains("TimeoutException")) {
                 Assert.assertTrue(false);
             }
