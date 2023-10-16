a7593556b7a362b66b661bf9d409e99dd214ed8b
Inderkant Pippal <>
2023-10-16 16:42:29 +0530

    TEST

--- Appium/src/main/java/Helpers/RewardsHelper.java
+++ Appium/src/main/java/Helpers/RewardsHelper.java
@@ -49,7 +49,7 @@ public class RewardsHelper {
 
         // Click the CTA - Swipe Up More
         screen.swipeUpMore(driver);
-        screen.swipeUpLess(driver);
+        screen.swipeUpMore(driver);
 
         if(rewardsPage.isSpinTheWheelPresent()){
 
