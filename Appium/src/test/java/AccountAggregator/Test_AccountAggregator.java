63ab6849ffe7e32b5ddc990ef721a5169a2fcb57
Mayank Suneja <mayank.suneja@mobikwik.com>
2023-11-03 17:41:19 +0530

    Made the code review changes

--- Appium/src/test/java/AccountAggregator/Test_AccountAggregator.java
+++ Appium/src/test/java/AccountAggregator/Test_AccountAggregator.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Test_AccountAggregator extends TestBase {
 
 
-    @Test(groups = {"AccountAggregator", "sanity"}, priority = 1, description = "Verify Existing User flow in AA")
+    @Test(groups = {"AccountAggregator", "sanity", "regression"}, priority = 0, description = "Verify Existing User flow in AA")
     public void Test01_existing_User_Dashboard() throws IOException, InterruptedException {
 
 
@@ -24,47 +24,47 @@ public class Test_AccountAggregator extends TestBase {
 
     }
 
-    @Test(groups = {"AccountAggregator", "sanity"}, priority = 1, description = "Verify Monthly summary")
+    @Test(groups = {"AccountAggregator", "sanity", "regression"}, priority = 0, description = "Verify Monthly summary")
     public void Test02_existing_User_MonthlySummery() throws IOException, InterruptedException {
 
         LoginHelper loginHelp = new LoginHelper(getAndroidDriver());
-        loginHelp.quickLoginViaOtp("7795709569" , "547372");
+        loginHelp.quickLoginViaOtp("7795709569", "547372");
         AAHelper aaHelper = new AAHelper(getAndroidDriver());
         // Execute the test
-        aaHelper.existingUserMonthlySummary("Incoming","Outgoing","Invested","Remaining","Outgoing","Expenses","SIPs & EMIs","Bank charges","Others","Highest Spend","Top Category","Spends by Date","Money Received");
+        aaHelper.existingUserMonthlySummary("Incoming", "Outgoing", "Invested", "Remaining", "Outgoing", "Expenses", "SIPs & EMIs", "Bank charges", "Others", "Highest Spend", "Top Category", "Spends by Date", "Money Received");
         Log.info("======= Account Aggregator test =======");
 
     }
 
-    @Test(groups = {"AccountAggregator", "sanity"}, priority = 1, description = "Verify Analyser")
+    @Test(groups = {"AccountAggregator", "sanity", "regression"}, priority = 0, description = "Verify Analyser")
     public void Test03_existing_User_Analyser() throws IOException, InterruptedException {
 
         LoginHelper loginHelp = new LoginHelper(getAndroidDriver());
-        loginHelp.quickLoginViaOtp("7795709569" , "547372");
+        loginHelp.quickLoginViaOtp("7795709569", "547372");
         AAHelper aaHelper = new AAHelper(getAndroidDriver());
         // Execute the test
-        aaHelper.existingUserAnalyser("DEBITS","Credits","Week","Month","Year","Debited this week");
+        aaHelper.existingUserAnalyser("DEBITS", "Credits", "Week", "Month", "Year", "Debited this week");
         Log.info("======= Account Aggregator test =======");
 
     }
 
-    @Test(groups = {"AccountAggregator", "sanity"}, priority = 1, description = "Verify Manage Section")
+    @Test(groups = {"AccountAggregator", "sanity", "regression"}, priority = 0, description = "Verify Manage Section")
     public void Test04_existing_User_Manage() throws IOException, InterruptedException {
 
         LoginHelper loginHelp = new LoginHelper(getAndroidDriver());
-        loginHelp.quickLoginViaOtp("7795709569" , "547372");
+        loginHelp.quickLoginViaOtp("7795709569", "547372");
         AAHelper aaHelper = new AAHelper(getAndroidDriver());
         // Execute the test
-        aaHelper.existingUserManage("Auto Refresh Frequency","Manage Consent","Help");
+        aaHelper.existingUserManage("Auto Refresh Frequency", "Manage Consent", "Help");
         Log.info("======= Account Aggregator test =======");
 
     }
 
-    @Test(groups = {"AccountAggregator", "sanity"}, priority = 0, description = "Verify Added Existing Users bank account")
+    @Test(groups = {"AccountAggregator", "sanity", "regression"}, priority = 0, description = "Verify Added Existing Users bank account")
     public void Test05_existing_User_Added_BankAccount() throws IOException, InterruptedException {
 
         LoginHelper loginHelp = new LoginHelper(getAndroidDriver());
-        loginHelp.quickLoginViaOtp("7795709569" , "547372");
+        loginHelp.quickLoginViaOtp("7795709569", "547372");
         AAHelper aaHelper = new AAHelper(getAndroidDriver());
         // Execute the test
         aaHelper.existingUserBankAccountDetails();
