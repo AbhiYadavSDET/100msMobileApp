21b6538c9da8d6604e1db8d48e4ff86b045c5b56
Paraj Jain <paraj.jain@mobikwik.com>
2024-01-24 18:52:45 +0530

    Added Zip Active Users and Zip Auto Pay Cases

--- Appium/src/main/java/PageObject/ZipPage.java
+++ Appium/src/main/java/PageObject/ZipPage.java
@@ -1,10 +1,13 @@
 package PageObject;
 
+import Utils.Element;
 import Utils.Elements;
+import com.aventstack.extentreports.gherkin.model.And;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 
 public class ZipPage {
@@ -38,6 +41,54 @@ public class ZipPage {
 
     @AndroidFindBy(id = "tv_label")
     private AndroidElement label;
+
+    @AndroidFindBy(id="balance")
+    private AndroidElement available_balance;
+
+    @AndroidFindBy(id="amount_used")
+    private AndroidElement amount_used;
+
+    @AndroidFindBy(id="amount_total")
+    private AndroidElement amount_total;
+
+    @AndroidFindBy(id="tv_credit_value")
+    private AndroidElement credit_value;
+
+    @AndroidFindBy(id="tv_cta")
+    private AndroidElement view_report_cta;
+
+    @AndroidFindBy(id="mkab_icon_1")
+    private AndroidElement navigate_back;
+
+    public String txn_history_date="date";
+
+    public String offer_brand_name="brand_name";
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='Manage now']")
+    private AndroidElement manage_now_cta;
+
+    @AndroidFindBy(id="fab_add_connection")
+    private AndroidElement add_biller_cta;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='Mobile']")
+    private AndroidElement mobile_category_icon;
+
+    @AndroidFindBy(id="switch_auto_pay")
+    private AndroidElement zip_auto_pay_toggle;
+
+    @AndroidFindBy(id="continue_button")
+    private AndroidElement save_connection_cta;
+
+    @AndroidFindBy(id = "et_search")
+    private AndroidElement search;
+
+    @AndroidFindBy(id = "operator_layout")
+    private AndroidElement selectAvailableResult;
+
+    @AndroidFindBy(id = "edit_text")
+    private AndroidElement mobileNumberEditText;
+
+
 
 
     public ZipPage(AndroidDriver driver) {
@@ -97,6 +148,85 @@ public class ZipPage {
     public String getLabel() throws InterruptedException {
         return Elements.getText(driver, label, "Label");
     }
+
+    public String getAvailableBalance() throws InterruptedException {
+        return Elements.getText(driver, available_balance).replace("Available Balance : ₹", "").replace(",","");
+    }
+
+    public String getAmountUsed() throws InterruptedException {
+        return Elements.getText(driver, amount_used).replace("₹", "").replace(",","");
+    }
+
+    public String getAmountTotal() throws InterruptedException {
+        return Elements.getText(driver, amount_total).replace("₹", "").replace(",","");
+    }
+
+    public String fetchCreditValue() throws InterruptedException {
+        return Elements.getText(driver, credit_value);
+    }
+
+    public void selectViewReport() throws InterruptedException{
+        Elements.selectElement(driver, view_report_cta, "Click on View Report cta for Credit Score");
+    }
+
+    public void navigateBack() throws InterruptedException{
+        Elements.selectElement(driver, navigate_back, "Navigating Back");
+    }
+
+    public int getHistoryCount() throws InterruptedException{
+        return Element.findElements(driver, By.id(txn_history_date)).size();
+
+    }
+
+    public int getOfferCount() throws InterruptedException{
+        return Element.findElements(driver, By.id(offer_brand_name)).size();
+
+    }
+
+    public void clickOnManageNowCta() throws InterruptedException{
+        Elements.selectElement(driver, manage_now_cta, "Click on Manage Now Cta");
+    }
+
+    public void clickOnAddBillerCta() throws InterruptedException{
+        Elements.selectElement(driver, add_biller_cta, "Click on Add Biller Cta");
+    }
+
+    public void selectMobileIcon() throws InterruptedException{
+        Elements.selectElement(driver, mobile_category_icon, "Click on Electricity");
+    }
+
+    public void clickOnAutopayToggle() throws InterruptedException{
+        Elements.selectElement(driver, zip_auto_pay_toggle, "Click on Autopay Toggle");
+    }
+
+    public void clickOnSaveConnectionCta() throws InterruptedException{
+        Elements.selectElement(driver, save_connection_cta, "Click on Save Connection Cta");
+    }
+
+    public void clickSearchBrand(){
+        Elements.selectElement(driver, search, "Click Search Brand");
+    }
+
+    public void clickSearchCity(){
+        Elements.selectElement(driver, search, "Click Search City");
+    }
+
+    public void enterSearchCriteria(String data) throws InterruptedException {
+        //Elements.isElementPresent(driver, searchElectricityBrand);
+        Elements.enterToElement(driver, search, data,"Enter Search Criteria");
+    }
+
+    public void clickSelectResult(){
+        Elements.selectElement(driver, selectAvailableResult, "Click on Available Result");
+    }
+
+    public void enterMobileNumber(String number) throws InterruptedException {
+        //Elements.isElementPresent(driver, caNumber);
+        Elements.enterToElement(driver, mobileNumberEditText, number, "Enter Mobile number");
+    }
+
+
+
 
 
 }
--- Appium/src/test/java/Zip/ZipTest.java
+++ Appium/src/test/java/Zip/ZipTest.java
@@ -30,4 +30,45 @@ public class ZipTest extends TestBase {
 
 
     }
+
+    @Test(groups = {"sanity", "verifyZipPageActiveUser", "regression"}, priority = 0, description = "Verify Zip Page for Active User")
+    public void Test_Zip_verifyZipPage_activeUser() throws IOException, InterruptedException {
+
+        Log.info("======= START : Zip Verify test for active User =======");
+
+
+        // Login to the account
+        LoginHelper loginHelper = new LoginHelper(getAndroidDriver());
+        loginHelper.quickLoginViaOtp("7795709569", "547372");
+
+        // Execute the test
+        ZipHelper zipHelper = new ZipHelper(getAndroidDriver());
+        zipHelper.verifyZipActiveUser("ZIP Dashboard","Your Credit Score");
+
+        Log.info("======= END : Zip Verify test for active User =======");
+
+
+    }
+
+    @Test(groups = {"sanity", "verifyZipAutoPay", "regression"}, priority = 0, description = "Verify Zip Auto Pay")
+    public void Test_Zip_verifyZip_AutoPay() throws IOException, InterruptedException {
+
+        Log.info("======= START : Zip Verify test for Zip Auto Pay =======");
+
+
+        // Login to the account
+        LoginHelper loginHelper = new LoginHelper(getAndroidDriver());
+        loginHelper.quickLoginViaOtp("7795709569", "547372");
+
+        // Execute the test
+        ZipHelper zipHelper = new ZipHelper(getAndroidDriver());
+        zipHelper.verifyZipAutoPay("ZIP Dashboard","Bill Book", "Vi", "Delhi NCR", "7795709569");
+
+        Log.info("======= END : Zip Verify test for Zip Auto Pay =======");
+
+
+    }
+
+
+
 }
