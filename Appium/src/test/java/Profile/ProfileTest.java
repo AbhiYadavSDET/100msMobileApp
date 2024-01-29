3936869df196e5454cd4bfaee5b8dfe4f62b89fc
Inderkant Pippal <inderkant.pippal@mobikwik.com>
2024-01-29 12:20:37 +0530

    Check IMPS flow

--- Appium/src/test/java/Profile/ProfileTest.java
+++ Appium/src/test/java/Profile/ProfileTest.java
@@ -1,6 +1,8 @@
 package Profile;
 
+import Helpers.LoginHelper;
 import Helpers.ProfileHelper;
+import Logger.Log;
 import Utils.TestBase;
 import org.testng.annotations.Test;
 
@@ -11,6 +13,11 @@ public class ProfileTest extends TestBase {
     @Test(groups = {"profileFlow"}, priority = 0, description = "Verify Profile Flow on android app")
     public void Test_Profile_Flow() throws InterruptedException, IOException {
         // Start the test
+        Log.info("======= START : Profile Section =======");
+
+        LoginHelper loginHelp = new LoginHelper(getAndroidDriver());
+        loginHelp.quickLoginViaOtp("9205299330","547372");
+
         ProfileHelper profileHelp = new ProfileHelper(getAndroidDriver());
         profileHelp.profileView("9818484290" ,"Udit Gupta", "uditgupta5j15@gmail.com");
 
