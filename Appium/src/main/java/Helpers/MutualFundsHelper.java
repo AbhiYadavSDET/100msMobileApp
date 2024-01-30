ee3d729e312f1e89d3061451de8ce2802fe57d03
Paraj Jain <paraj.jain@mobikwik.com>
2024-01-30 21:07:45 +0530

    Fixed Mutual Funds Case

--- Appium/src/main/java/Helpers/MutualFundsHelper.java
+++ Appium/src/main/java/Helpers/MutualFundsHelper.java
@@ -57,6 +57,7 @@ public class MutualFundsHelper {
 
         // Swipe till the bottom
         screen.swipeUpMore(driver);
+        screen.swipeUpMore(driver);
         screen.swipeUpLess(driver);
 
 
