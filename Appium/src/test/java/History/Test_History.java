76c35c3f40470b481b8df83b07f967c37209b154
OMK903 <34240491+OMK903@users.noreply.github.com>
2024-03-13 18:09:47 +0530

    History code coverage

--- Appium/src/test/java/History/Test_History.java
+++ Appium/src/test/java/History/Test_History.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Test_History extends TestBase {
 
 
-    /*@Test(groups = {"History", "sanity", "regression"}, priority = 2, description = "Verify New User History")
+    @Test(groups = {"History", "sanity", "regression"}, priority = 2, description = "Verify New User History")
     public void Test01_History_New_User() throws IOException, InterruptedException {
         LoginHelper loginHelp = new LoginHelper(getAndroidDriver());
         loginHelp.quickLoginViaOtp("8216900006", "547372");
@@ -20,7 +20,7 @@ public class Test_History extends TestBase {
         historyHelper.newUserHistory();
         Log.info("======= History test =======");
 
-    }*/
+    }
 
     @Test(groups = {"History", "sanity", "regression"}, priority = 1, description = "Verify Existing User History")
     public void Test02_History_Kyced_User() throws IOException, InterruptedException {
@@ -34,7 +34,7 @@ public class Test_History extends TestBase {
     }
 
 
-    /*@Test(groups = {"History", "sanity", "regression"}, priority = 1, description = "Verify Existing User History")
+    @Test(groups = {"History", "sanity", "regression"}, priority = 1, description = "Verify Existing User History")
     public void Test03_History_Kyced_User_AAbottomsheet() throws IOException, InterruptedException {
         LoginHelper loginHelp = new LoginHelper(getAndroidDriver());
         loginHelp.quickLoginViaOtp("7042338867", "547372");
@@ -43,6 +43,6 @@ public class Test_History extends TestBase {
         historyHelper.existingUserHistoryAAbottomSheet("SELECT YOUR BANK","Insights");
         Log.info("======= History test Kyced User =======");
 
-    }*/
+    }
 
 }
