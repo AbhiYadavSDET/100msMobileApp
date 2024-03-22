d1aec418b0cc651f20158f2c018e9ad790081340
Inderkant Pippal <inderkant.pippal@mobikwik.com>
2024-03-22 20:04:53 +0530

    Refer And Earn New Cases

---
+++ Appium/src/test/java/ReferAndEarn/ReferAndEarnTest.java
@@ -0,0 +1,61 @@
+package ReferAndEarn;
+
+import Helpers.LoginHelper;
+import Helpers.ReferAndEarnHelper;
+import Logger.Log;
+import Utils.TestBase;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class ReferAndEarnTest extends TestBase {
+
+    @Test(groups = {"referInUpiWithoutEarnings"}, priority = 0, description = "Verify Refer & Earn UPI with NO Earnings")
+    public void Test01_Refer_And_Earn_UPI() throws InterruptedException, IOException {
+
+        Log.info("======= START : Refer And Earn in UPI with no earnings=======");
+
+        // Start the test
+        LoginHelper loginHelp = new LoginHelper(getAndroidDriver());
+        loginHelp.quickLoginViaOtp("9205299330", "547372");
+
+        ReferAndEarnHelper referAndEarnHelper = new ReferAndEarnHelper(getAndroidDriver());
+        referAndEarnHelper.referInUpiWithoutEarnings("Refer & earn ₹20");
+
+        Log.info("======= END : Refer And Earn in UPI with no earnings =======");
+
+    }
+
+
+    @Test(groups = {"referInUpiWithEarnings"}, priority = 1, description = "Verify Refer & Earn UPI with Earnings table")
+    public void Test02_Refer_And_Earn_WithEarning_UPI() throws InterruptedException, IOException {
+
+        Log.info("======= START : Refer And Earn in UPI with earnings=======");
+
+        // Start the test
+        LoginHelper loginHelp = new LoginHelper(getAndroidDriver());
+        loginHelp.quickLoginViaOtp("7795709569", "547372");
+
+        ReferAndEarnHelper referAndEarnHelper = new ReferAndEarnHelper(getAndroidDriver());
+        referAndEarnHelper.referInUpiWithEarnings("Refer & earn ₹20");
+
+        Log.info("======= END : Refer And Earn in UPI earnings =======");
+    }
+
+
+    @Test(groups = {"referInCCBP"}, priority = 2, description = "Verify Refer & Earn in CCBP")
+    public void Test03_Refer_And_Earn_CCBP() throws InterruptedException, IOException {
+
+        Log.info("======= START : Refer And Earn in CCBP =======");
+
+        // Start the test
+        LoginHelper loginHelp = new LoginHelper(getAndroidDriver());
+        loginHelp.quickLoginViaOtp("9205299330", "547372");
+
+        ReferAndEarnHelper referAndEarnHelper = new ReferAndEarnHelper(getAndroidDriver());
+        referAndEarnHelper.referInCCBP();
+
+        Log.info("======= END : Refer And Earn in CCBP =======");
+    }
+
+}
