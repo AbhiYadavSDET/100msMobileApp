def2f0a7cc050150f08d86b1f18ce08a05f90682
Inderkant Pippal <inderkant.pippal@mobikwik.com>
2024-07-04 17:46:59 +0530

    2-FA

--- Appium/src/main/java/Helpers/AddMoneyHelper.java
+++ Appium/src/main/java/Helpers/AddMoneyHelper.java
@@ -177,7 +177,7 @@ public class AddMoneyHelper {
 
             addMoneyPage.clickOnPay();
 
-            Thread.sleep(3000);
+            Thread.sleep(5000);
 
             //2FA Validation Page Assertions
            // Element.waitForVisibility(driver, By.id("btn_verify"));
