056e1f5bd28c25c3788b88e6770090f426f917b7
Inderkant Pippal <inderkant.pippal@mobikwik.com>
2024-07-04 19:36:07 +0530

    Upi

--- Appium/src/main/java/Helpers/AddMoneyHelper.java
+++ Appium/src/main/java/Helpers/AddMoneyHelper.java
@@ -9,9 +9,12 @@ import Logger.Log;
 import Utils.MBReporter;
 import Utils.Screen;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
 
+import java.time.Duration;
 import java.util.HashMap;
 
 public class AddMoneyHelper {
@@ -177,11 +180,12 @@ public class AddMoneyHelper {
 
             addMoneyPage.clickOnPay();
 
-            Thread.sleep(5000);
+//            WebDriverWait wait = new WebDriverWait(driver, 10);
+//            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btn_verify")));
 
             //2FA Validation Page Assertions
-           // Element.waitForVisibility(driver, By.id("btn_verify"));
-            mbkCommonControlsHelper.handle2FADeviceBindingFlow();
+            Element.waitForVisibility(driver, By.id("btn_verify"));
+            mbkCommonControlsHelper.handle2FADeviceBindingFlow(true);
 
             mbkCommonControlsHelper.handleUpiPin(pin);
 
